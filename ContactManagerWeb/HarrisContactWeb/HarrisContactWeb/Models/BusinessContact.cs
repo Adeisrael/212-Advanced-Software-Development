b5bdb66c721637b6a5dab2b6b1218dcf9bfeea97
Adeisrael <52067445+Adeisrael@users.noreply.github.com>
2020-04-04 14:17:35 +0000

    ASP.Net Core Web Application
    Contact Manager built in ASP.Net Web with CRUD Functionalities

---
+++ ContactManagerWeb/HarrisContactWeb/HarrisContactWeb/Models/BusinessContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisContactWeb.Models
+{
+    public class BusinessContact
+    {//Attributes of BusinessContact Class
+        public int BusinessContactID { get; set; }//Primary key for Business Contacts
+        public string BusinessFname { get; set; }//String Business Contact Firstname
+        public string BusinessLname { get; set; }//String Business Contact Lastname 
+        public string BusinessEmail { get; set; }//String Business Contact Email
+        public string BusinessTel { get; set; }//String Business Contact Telephone
+        public string BusinessAdrr1 { get; set; }//String Business Contact Address 1
+        public string BusinessAddr2 { get; set; }//String Business Contact Address 2
+        public string BusinessCity { get; set; }//String Business Contact City
+        public string BuisnessPostCode { get; set; }//String Business Contact Postcode
+
+    }
+}
---
+++ ContactManagerWeb/HarrisContactWeb/HarrisContactWeb/Models/PersonalContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisContactWeb.Models
+{
+    public class PersonalContact
+    {// Attributes of the Personal Contact
+        public int PersonalContactID { get; set; }//Primary Key of Personal Contact
+        public string PersonalFname { get; set; }//String Personal Contact Firstname
+        public string PersonsalLname { get; set; }//String Personal Contact Lastname 
+        public string PersonalEmail { get; set; }//String Personal Contact Email
+        public string HomeTel { get; set; }//String Personal Contact Home Telephone
+        public string PersonalAddr1 { get; set; }//String Personal Contact Address 1
+        public string PersonalAddr2 { get; set; }//String Personal Contact Address 2
+        public string PersonalCity { get; set; }//String Personal Contact City 
+        public string PersonalPostCode { get; set; }//String Personal Contact PostCode
+
+    }
+}
